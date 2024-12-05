63a061e2ab90379f5d6bc773727c34398159b75d
franciscos <franciscos@pagotic.com>
2024-12-05 00:56:55 -0300

    se agrega el main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import sys, os
+sys.path.insert(0,"../..")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flecha_lexer import Flecha
 
+if __name__ == "__main__":
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    if len(sys.argv) != 2:
+        sys.stderr.write('Error de uso. Ejemplo: %s archivo_input: \n' % (sys.argv[0],))
+        sys.exit(1)
 
+    flecha = Flecha()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    data = f.read()
+    f.close()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    bare_filename = filename.split("/")[1].replace('.input', '')
+    flecha_filename = "tests_results/" + bare_filename + '.json'
+
+    program = flecha.yacc.parse(data)
+
+    f = open(flecha_filename, 'w')
+    f.write(str(program))
+    f.close()
